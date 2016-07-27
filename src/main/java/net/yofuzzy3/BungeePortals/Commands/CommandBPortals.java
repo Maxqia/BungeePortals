a272b32cbcff795619979af7db4a00188e686d4e
Max Qian <public@maxqia.com>
2016-07-27 06:32:38 -0700

    Refactor CommandBPortals

--- src/main/java/net/yofuzzy3/BungeePortals/Commands/CommandBPortals.java
+++ src/main/java/net/yofuzzy3/BungeePortals/Commands/CommandBPortals.java
@@ -29,141 +29,73 @@ public class CommandBPortals implements CommandExecutor {
     public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
         if (commandLabel.equalsIgnoreCase("BPortals")) {
             if (sender.hasPermission("BungeePortals.command.BPortals")) {
-                if (args.length == 0) {
-                    sender.sendMessage(ChatColor.BLUE + "BungeePortals v" + plugin.getDescription().getVersion() + " by YoFuzzy3");
-                    sender.sendMessage(ChatColor.GREEN + "/BPortals reload " + ChatColor.RED + "Reload all files and data.");
-                    sender.sendMessage(ChatColor.GREEN + "/BPortals forcesave " + ChatColor.RED + "Force-save portals.");
-                    sender.sendMessage(ChatColor.GREEN + "/BPortals select <filter,list> " + ChatColor.RED + "Get selection.");
-                    sender.sendMessage(ChatColor.GREEN + "/BPortals clear " + ChatColor.RED + "Clear selection.");
-                    sender.sendMessage(ChatColor.GREEN + "/BPortals create <destination> " + ChatColor.RED + "Create portals.");
-                    sender.sendMessage(ChatColor.GREEN + "/BPortals remove <destination> " + ChatColor.RED + "Remove portals.");
-                    sender.sendMessage(ChatColor.BLUE + "Visit www.spigotmc.org/resources/bungeeportals.19 for help.");
-                } else if (args.length == 1) {
-                    if (args[0].equalsIgnoreCase("reload")) {
-                        plugin.loadConfigFiles();
-                        plugin.loadPortalsData();
-                        sender.sendMessage(ChatColor.GREEN + "All configuration files and data have been reloaded.");
-                    } else if (args[0].equalsIgnoreCase("forcesave")) {
-                        plugin.savePortalsData();
-                        sender.sendMessage(ChatColor.GREEN + "Portal data saved!");
-                    } else if (args[0].equalsIgnoreCase("clear")) {
-                        if (sender instanceof Player) {
-                            String playerName = sender.getName();
-                            if (selections.containsKey(playerName)) {
-                                selections.remove(playerName);
-                                sender.sendMessage(ChatColor.GREEN + "Selection cleared.");
-                            } else {
-                                sender.sendMessage(ChatColor.RED + "You haven't selected anything.");
-                            }
-                        } else {
-                            sender.sendMessage(ChatColor.RED + "Only players can use that command.");
-                        }
-                    } else {
-                        sender.sendMessage(ChatColor.RED + "Type /BPortals for help!");
-                    }
-                } else if (args.length == 2) {
-                    if (args[0].equalsIgnoreCase("select")) {
-                        if (sender instanceof Player) {
-                            Player player = (Player) sender;
-                            String playerName = player.getName();
-                            Selection selection = plugin.worldEdit.getSelection(player);
-                            if (selection != null) {
-                                if (selection instanceof CuboidSelection) {
-                                    List<Location> locations = getLocationsFromCuboid((CuboidSelection) selection);
-                                    List<String> blocks = new ArrayList<>();
-                                    String[] ids = {};
-                                    int count = 0;
-                                    int filtered = 0;
-                                    boolean filter = false;
-                                    if (!args[1].equals("0")) {
-                                        ids = args[1].split(",");
-                                        filter = true;
+                if (sender instanceof Player) {
+                    Player player = (Player) sender;
+                    String playerName = sender.getName();
+                    if (args.length >= 1) {
+                        switch (args[0].toLowerCase()) {
+                            case "select": select(player,args); break;
+                            case "reload":
+                                plugin.loadConfigFiles();
+                                plugin.loadPortalsData();
+                                sender.sendMessage(ChatColor.GREEN + "All configuration files and data have been reloaded.");
+                                break;
+                            case "forcesave":
+                                plugin.savePortalsData();
+                                sender.sendMessage(ChatColor.GREEN + "Portal data saved!");
+                                break;
+                            case "clear":
+                                if (selections.containsKey(playerName)) {
+                                    selections.remove(playerName);
+                                    sender.sendMessage(ChatColor.GREEN + "Selection cleared.");
+                                } else {
+                                    sender.sendMessage(ChatColor.RED + "You haven't selected anything.");
+                                }
+                                break;
+                            case "create":
+                                if (selections.containsKey(playerName)) {
+                                    List<String> selection = selections.get(playerName);
+                                    for (String block : selection) {
+                                        plugin.portalData.put(block, args[1]);
                                     }
-                                    for (Location location : locations) {
-                                        Block block = player.getWorld().getBlockAt(location);
-                                        if (filter) {
-                                            boolean found = false;
-                                            for (int i = 0; i < ids.length; i++) {
-                                                String[] parts = ids[i].split(":");
-                                                if (parts.length == 2) {
-                                                    if (parts[0].equals(String.valueOf(block.getTypeId())) && parts[1].equals(String.valueOf(block.getData()))) {
-                                                        found = true;
-                                                        break;
-                                                    }
-                                                } else {
-                                                    if (parts[0].equals(String.valueOf(block.getTypeId()))) {
-                                                        found = true;
-                                                        break;
-                                                    }
-                                                }
-                                            }
-                                            if (found) {
-                                                blocks.add(block.getWorld().getName() + "#" + String.valueOf(block.getX()) + "#" + String.valueOf(block.getY()) + "#" + String.valueOf(block.getZ()));
-                                                count++;
-                                            } else {
-                                                filtered++;
-                                            }
-                                        } else {
-                                            blocks.add(block.getWorld().getName() + "#" + String.valueOf(block.getX()) + "#" + String.valueOf(block.getY()) + "#" + String.valueOf(block.getZ()));
+                                    player.sendMessage(ChatColor.GREEN + String.valueOf(selection.size()) + " portals have been created.");
+                                } else {
+                                    player.sendMessage(ChatColor.RED + "You haven't selected anything.");
+                                }
+                                break;
+                            case "remove":
+                                if (selections.containsKey(playerName)) {
+                                    int count = 0;
+                                    for (String block : selections.get(playerName)) {
+                                        if (plugin.portalData.containsKey(block)) {
+                                            plugin.portalData.remove(block);
                                             count++;
                                         }
                                     }
-                                    selections.put(playerName, blocks);
-                                    sender.sendMessage(ChatColor.GREEN + String.valueOf(count) + " blocks have been selected, " + String.valueOf(filtered) + " filtered.");
-                                    sender.sendMessage(ChatColor.GREEN + "Use the selection in the create and remove commands.");
+                                    sender.sendMessage(ChatColor.GREEN + String.valueOf(count) + " portals have been removed.");
                                 } else {
-                                    sender.sendMessage(ChatColor.RED + "Must be a cuboid selection!");
+                                    sender.sendMessage(ChatColor.RED + "You haven't selected anything.");
                                 }
-                            } else {
-                                sender.sendMessage(ChatColor.RED + "You have to first create a WorldEdit selection!");
-                            }
-                        } else {
-                            sender.sendMessage(ChatColor.RED + "Only players can use that command.");
+                                break;
+                            default: help(sender);
                         }
-                    } else if (args[0].equalsIgnoreCase("create")) {
-                        if (sender instanceof Player) {
-                            String playerName = sender.getName();
-                            if (selections.containsKey(playerName)) {
-                                List<String> selection = selections.get(playerName);
-                                for (String block : selection) {
-                                    plugin.portalData.put(block, args[1]);
-                                }
-                                sender.sendMessage(ChatColor.GREEN + String.valueOf(selection.size()) + " portals have been created.");
-                            } else {
-                                sender.sendMessage(ChatColor.RED + "You haven't selected anything.");
-                            }
-                        } else {
-                            sender.sendMessage(ChatColor.RED + "Only players can use that command.");
-                        }
-                    } else if (args[0].equalsIgnoreCase("remove")) {
-                        if (sender instanceof Player) {
-                            String playerName = sender.getName();
-                            if (selections.containsKey(playerName)) {
-                                int count = 0;
-                                for (String block : selections.get(playerName)) {
-                                    if (plugin.portalData.containsKey(block)) {
-                                        plugin.portalData.remove(block);
-                                        count++;
-                                    }
-                                }
-                                sender.sendMessage(ChatColor.GREEN + String.valueOf(count) + " portals have been removed.");
-                            } else {
-                                sender.sendMessage(ChatColor.RED + "You haven't selected anything.");
-                            }
-                        } else {
-                            sender.sendMessage(ChatColor.RED + "Only players can use that command.");
-                        }
-                    } else {
-                        sender.sendMessage(ChatColor.RED + "Type /BPortals for help!");
-                    }
-                } else {
-                    sender.sendMessage(ChatColor.RED + "Type /BPortals for help!");
-                }
-            } else {
-                sender.sendMessage(ChatColor.RED + "You do not have permission to use that command.");
-            }
+                    } else help(sender);
+                } else sender.sendMessage(ChatColor.RED + "Only players can use this command.");
+            } else sender.sendMessage(ChatColor.RED + "You do not have permission to use that command.");
+            return true;
         }
-        return true;
+        return false;
+    }
+
+    private void help(CommandSender sender) {
+        sender.sendMessage(ChatColor.BLUE + "BungeePortals v" + plugin.getDescription().getVersion() + " by YoFuzzy3");
+        sender.sendMessage(ChatColor.GREEN + "/BPortals reload " + ChatColor.RED + "Reload all files and data.");
+        sender.sendMessage(ChatColor.GREEN + "/BPortals forcesave " + ChatColor.RED + "Force-save portals.");
+        sender.sendMessage(ChatColor.GREEN + "/BPortals select <filter,list> " + ChatColor.RED + "Get selection.");
+        sender.sendMessage(ChatColor.GREEN + "/BPortals clear " + ChatColor.RED + "Clear selection.");
+        sender.sendMessage(ChatColor.GREEN + "/BPortals create <destination> " + ChatColor.RED + "Create portals.");
+        sender.sendMessage(ChatColor.GREEN + "/BPortals remove <destination> " + ChatColor.RED + "Remove portals.");
+        sender.sendMessage(ChatColor.BLUE + "Visit www.spigotmc.org/resources/bungeeportals.19 for help.");
     }
 
     private List<Location> getLocationsFromCuboid(CuboidSelection cuboid) {
@@ -180,4 +112,59 @@ public class CommandBPortals implements CommandExecutor {
         return locations;
     }
 
+    private void select(CommandSender sender, String[] args) {
+        Player player = (Player) sender;
+        String playerName = player.getName();
+        Selection selection = plugin.worldEdit.getSelection(player);
+        if (selection != null) {
+            if (selection instanceof CuboidSelection) {
+                List<Location> locations = getLocationsFromCuboid((CuboidSelection) selection);
+                List<String> blocks = new ArrayList<>();
+                String[] ids = {};
+                int count = 0;
+                int filtered = 0;
+                boolean filter = false;
+                if (!args[1].equals("0")) {
+                    ids = args[1].split(",");
+                    filter = true;
+                }
+                for (Location location : locations) {
+                    Block block = player.getWorld().getBlockAt(location);
+                    if (filter) {
+                        boolean found = false;
+                        for (int i = 0; i < ids.length; i++) {
+                            String[] parts = ids[i].split(":");
+                            if (parts.length == 2) {
+                                if (parts[0].equals(String.valueOf(block.getTypeId())) && parts[1].equals(String.valueOf(block.getData()))) {
+                                    found = true;
+                                    break;
+                                }
+                            } else {
+                                if (parts[0].equals(String.valueOf(block.getTypeId()))) {
+                                    found = true;
+                                    break;
+                                }
+                            }
+                        }
+                        if (found) {
+                            blocks.add(block.getWorld().getName() + "#" + String.valueOf(block.getX()) + "#" + String.valueOf(block.getY()) + "#" + String.valueOf(block.getZ()));
+                            count++;
+                        } else {
+                            filtered++;
+                        }
+                    } else {
+                        blocks.add(block.getWorld().getName() + "#" + String.valueOf(block.getX()) + "#" + String.valueOf(block.getY()) + "#" + String.valueOf(block.getZ()));
+                        count++;
+                    }
+                }
+                selections.put(playerName, blocks);
+                sender.sendMessage(ChatColor.GREEN + String.valueOf(count) + " blocks have been selected, " + String.valueOf(filtered) + " filtered.");
+                sender.sendMessage(ChatColor.GREEN + "Use the selection in the create and remove commands.");
+            } else {
+                sender.sendMessage(ChatColor.RED + "Must be a cuboid selection!");
+            }
+        } else {
+            sender.sendMessage(ChatColor.RED + "You have to first create a WorldEdit selection!");
+        }
+    }
 }
